bf1fc31c68e9e2d8a2f4bd02dcd55faea25adfaa
Danny Sanchez <dsanchez910@gmail.com>
2019-11-02 20:15:07 -0400

    created the router for the burgers. used get post and put for creating the burger, updating the burger, and I exported the router at the end

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const burger = require("../models/burger.js");
+const router = express.Router();
+
+router.get('/',function(req,res){
+    res.redirect("/burgers");
+});
+
+router.get('/burgers',function(req,res){
+    burger.select(function(data){
+        var hbsObject = { burgers: data };
+        res.render('index',hbsObject);
+    });
+});
+
+router.post("/burgers/create",function(req,res){
+    burger.create(["burger_name"],[req.body.burger_name],function(result){
+        res.redirect("/burgers");
+    });
+});
+
+router.put('/burgers/update/:id', function(req,res){
+    var condition = `id = ${req.params.id}`;
+    burger.update({ 'devoured': req.body.devoured },condition,function(data){
+        res.redirect('/burgers');
+    });
+});
+
+module.exports = router;
